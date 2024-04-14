b56328e3790f66eac9db19c484c5cc4820192015
khaingmyozaw <kmyozaw.dev@gmail.com>
2024-04-14 11:20:59 +0630

    made scroll reveal and download cv

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 
+// CV form preview and download
+const link = 'https://drive.google.com/uc?export=download&id=1HP4wm_5m7c_Dgtiw-yi9fSiLl9WrXoZy';
+
+function downloadCV() {
+    location.href = link;
+    // console.log('hello')
+}
+
 // navbar animation when home page is scrolled
 window.addEventListener('scroll', function() {
     if(this.scrollY < $('#home').height()) {
@@ -46,6 +54,7 @@ $(document).ready(function() {
         // $('.see-more').toggleClass('bg-dark');
         $('.content').toggleClass('text-white-50');
         $('.content').toggleClass('text-black-50');
+        $('.social-medias').toggleClass('text-white');
         
         if(bol) {
             bol = false;
@@ -58,3 +67,46 @@ $(document).ready(function() {
     })
 
 })
+
+
+// Scroll Reveal Animation
+
+ScrollReveal({
+    // reset: true,
+    distance: '30px',
+    duration: 2000,
+    delay: 400,
+})
+
+//
+ScrollReveal().reveal('#navbar-section', {delay: 500,origin: 'top',interval: 200,})
+
+// social medias
+ScrollReveal().reveal('.social-medias', {delay: 500, origin: 'right', interval: '200'});
+
+// profile
+ScrollReveal().reveal('.profile', {delay: 300,});
+
+//Name and others
+ScrollReveal().reveal('.myname, .zeembi', {delay: 500, origin: 'right', interval: '200'});
+ScrollReveal().reveal('.name, .web-developer', {delay: 600, origin: 'left',});
+ScrollReveal().reveal('.contacts, .see-more', {delay: 700, origin: 'bottom', interval: '200'});
+ScrollReveal().reveal('.contacts-span, .seemore-span', {delay: 700, origin: 'bottom', interval: '200'});
+
+// Hobby Section
+ScrollReveal().reveal('.hobby p', { delay: 300, origin: 'left',});
+ScrollReveal().reveal('.hobby-items', {delay: 400, origin: 'bottom', interval: '200'});
+
+// About Section
+ScrollReveal().reveal('.about h4', { delay: 300, origin: 'left',});
+ScrollReveal().reveal('.about-content', { delay: 400, origin: 'bottom',});
+ScrollReveal().reveal('.about-contacts i', {delay: 400, origin: 'left', interval: '200'});
+ScrollReveal().reveal('.about-contacts span', {delay: 600, origin: 'bottom', interval: '200'});
+
+// Skills and Education Section 
+ScrollReveal().reveal('.skill-lefts', {delay: 300, origin: 'left', interval: '150'});
+ScrollReveal().reveal('.skill-bottoms', {delay: 400, origin: 'bottom', interval: '150'});
+
+// Projects Section
+ScrollReveal().reveal('.projects h3', { delay: 300, origin: 'left',});
+ScrollReveal().reveal('.project-items', {delay: 500, origin: 'bottom', interval: '150'});
